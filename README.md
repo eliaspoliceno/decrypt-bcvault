dd359a2f1f62e4d03ea7fd77fb2b338be478e001
Elias Policena <eliaspoliceno@gmail.com>
2021-07-09 17:08:44 -0300

    Added BCrypt and Scrypt key specs to TesteAleatorio class, with a new MODE variable to control which one to use. No SCrypt combinations decrypted the bytes block. Two BCrypt combinations decrypted the block and also the inner block, again, i didn't know what i am seeing in these byte arrays. Comes to me that the decrypted block have 239 bytes, and this number should be multiple of 16 to be another inner AES-256 CBC block. Then, i added an offset from the beginning of the inner block to try to decrypt it.
    Signed-off-by: Elias Policena <eliaspoliceno@gmail.com>

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ By knowing Global Pass, Global PIN, and another wallets passwords and PINs, i ca
 Offcourse, this depends on discovering which encryption method these backup uses, which lead me to asking for help.
 
 # Firmware analysis
-I have downloaded the firmware files from these urls:
+The firmware is available for public download at these official urls:
 [https://update.bc-vault.com/downloads/firmware_1.5.6.bin](https://update.bc-vault.com/downloads/firmware_1.5.6.bin)
 [https://update.bc-vault.com/downloads/firmware_1.5.7.bin](https://update.bc-vault.com/downloads/firmware_1.5.7.bin)
 [https://update.bc-vault.com/downloads/firmware_1.5.9.bin](https://update.bc-vault.com/downloads/firmware_1.5.9.bin)
@@ -74,8 +74,13 @@ java -jar target/try-decrypt-0.0.1-SNAPSHOT.jar |tee target/results.txt
 1. CommandLineRunner: First try, its a garbage, just ignore.
 2. FindIVRunner: another mess, just ignore it too.
 3. AnotherCommandLineRunner: I tought the private key were someway just encrypted in these 256-byte blocks, so i revealed one of them and tryed to encrypt, then compared with the binary file content. Too much slow, won't run in a amount of time i can wait for. After it, i minded that private keys are not simplely encrypted, because how could the desktop application know these names we give and generated addresses of the wallets?
-4. <u>TesteAleatorio: ok, here things becomes interesting</u>. You can control it by RUN, RUN_OLD and RUN_NEW static booleans. In RUN_OLD i created a PBKDF2WithHmacSHA256 key with pass and pin (used as a salt), varying interactions from 0 to 100000 and tryed to decrypt the block. I do several tests, like, decrypted twice, exchaging pass and pin, tried base58 and base64 reading, reversed the block byte array, etc. **There were 1056 combinations that decrypted the file!** After that, i compiled the results (output/results.csv), which are: several interaction amounts can decrypt the block; the block is not twice encrypted; the block is not a base58 nor base64 encoded.
+4. *TesteAleatorio: ok, here things becomes interesting*. You can control it by RUN, RUN_OLD and RUN_NEW static booleans. In RUN_OLD i created a PBKDF2WithHmacSHA256 key with pass and pin (used as a salt), varying interactions from 0 to 100000 and tryed to decrypt the block. I do several tests, like, decrypted twice, exchaging pass and pin, tried base58 and base64 reading, reversed the block byte array, etc. **There were 1646 combinations that decrypted the file!** After that, i compiled the results (output/results.csv), which are: several interaction amounts can decrypt the block; the block is not twice encrypted; the block is not a base58 nor base64 encoded.
 5. Again in TesteAleatorio, now with RUN_NEW mode: now i got these interaction numbers in a csv file, i read the block and tryed to guess which encoding the decrypted block uses. Different interaction numbers produces same UTF-8 result (visually equals, i cannot guarantee), not human readable.
+
+# Updates
+Added BCrypt and Scrypt key specs to *TesteAleatorio* class, with a new *MODE* variable to control which one to use. No SCrypt combinations decrypted the bytes block.
+Two BCrypt combinations decrypted the block and also the inner block, again, i didn't know what i am seeing in these byte arrays.
+Comes to me that the decrypted block have 239 bytes, and this number should be multiple of 16 to be another inner AES-256 CBC block. Then, i added an offset from the beginning of the inner block to try to decrypt it.
 
 # Help need
 Ok guys, now i am asking for help of you experts. Get in touch if interested.
--- try-decrypt/src/main/java/br/org/policena/trydecrypt/TesteAleatorio.java
+++ try-decrypt/src/main/java/br/org/policena/trydecrypt/TesteAleatorio.java
@@ -25,6 +25,8 @@ import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.bouncycastle.crypto.generators.BCrypt;
+import org.bouncycastle.crypto.generators.SCrypt;
 import org.mozilla.universalchardet.UniversalDetector;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -37,17 +39,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TesteAleatorio implements CommandLineRunner {
 
 	private static final boolean RUN = Boolean.TRUE;
-	private static final boolean RUN_OLD = Boolean.FALSE;
-	private static final boolean RUN_NEW = Boolean.TRUE;
+	private static final boolean RUN_OLD = Boolean.TRUE;
+	private static final boolean RUN_NEW = Boolean.FALSE;
+	
+	private static int MODE = 1; // 0 for PBKDF2WithHmacSHA256, 1 for BCrypt, 2 for Scrypt
+	
 	private ExecutorService pool;
 	LinkedBlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>();
 
 	@Override
 	public void run(String... args) throws Exception {
 		if (RUN) {
+			pool = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(),
+					Runtime.getRuntime().availableProcessors(), 0L, TimeUnit.MILLISECONDS, queue);
 			if (RUN_OLD) {
-				pool = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(),
-						Runtime.getRuntime().availableProcessors(), 0L, TimeUnit.MILLISECONDS, queue);
 
 				// BalloonHash bh = new
 				// BalloonHash(MessageDigest.getInstance("SHA-256"), 1 << 6, 1
@@ -98,33 +103,50 @@ public class TesteAleatorio implements CommandLineRunner {
 
 				SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
 				records.forEach(record -> {
-					char[] pass = record.get(0).trim().toCharArray();
-					byte[] pin = record.get(1).trim().getBytes();
-					int interactions = Integer.valueOf(record.get(2).trim());
-					boolean reverse = Boolean.valueOf(record.get(3).trim());
-					KeySpec spec = new PBEKeySpec(pass, pin, interactions, 256);
-					try {
-						SecretKey key = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
-
-						Cipher cipher;
-						cipher = Cipher.getInstance(AESUtil.CIPHER_INSTANCE);
-						cipher.init(Cipher.DECRYPT_MODE, key, iv);
-
-						byte[] toDecrypt = blockAes;
-						if (reverse) {
-							byte[] localBytes = Arrays.copyOf(blockAes, blockAes.length);
-							ArrayUtils.reverse(localBytes);
-							toDecrypt = localBytes;
+					pool.submit(() -> {
+						char[] pass = record.get(0).trim().toCharArray();
+						byte[] pin = record.get(1).trim().getBytes();
+						int interactions = Integer.valueOf(record.get(2).trim());
+						boolean reverse = Boolean.valueOf(record.get(3).trim());
+						KeySpec spec = new PBEKeySpec(pass, pin, interactions, 256);
+						try {
+							SecretKey key = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+							
+							Cipher cipher;
+							cipher = Cipher.getInstance(AESUtil.CIPHER_INSTANCE);
+							cipher.init(Cipher.DECRYPT_MODE, key, iv);
+							
+							byte[] toDecrypt = blockAes;
+							if (reverse) {
+								byte[] localBytes = Arrays.copyOf(blockAes, blockAes.length);
+								ArrayUtils.reverse(localBytes);
+								toDecrypt = localBytes;
+							}
+							
+							byte[] decrypted = cipher.doFinal(toDecrypt);
+							String encoding = guessEncoding(decrypted);
+							String decoded = new String(toDecrypt, encoding);
+							
+							System.out.println(String.format("%s;%s;%d;%s;%s", new String(pass), new String(pin), interactions, encoding, decoded));
+							
+							// lines above print the inner block decrypted string
+//							 int offset = decrypted.length - 224;
+//							 byte[] newBlock = Arrays.copyOfRange(decrypted, offset, decrypted.length);
+							
+//							 byte[] newDecrypted = cipher.doFinal(newBlock);
+//							 String innerString = new String(newDecrypted, guessEncoding(newDecrypted));
+//							 System.out.println(String.format("Pass \"%s\" + PIN \"%s\" reverse \"%s\" decrypted the inner block! ",
+//									 record.get(0).trim(), record.get(1).trim(), record.get(3).trim()));
+//							 System.out.println(innerString);
+							
+						} catch (Exception e) {
 						}
-						
-						String encoding = guessEncoding(cipher.doFinal(toDecrypt));
-						String decoded = new String(toDecrypt, encoding);
-						System.out.println(String.format("%s;%s;%d;%s;%s", new String(pass), new String(pin), interactions, encoding, decoded));
-
-					} catch (Exception e) {
-					}
+					});
 				});
 			}
+			
+			pool.shutdown();
+			pool.awaitTermination(1, TimeUnit.DAYS);
 		}
 	}
 
@@ -152,12 +174,37 @@ public class TesteAleatorio implements CommandLineRunner {
 			throws Exception {
 		SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
 
-		for (int i = 0; i < 100_000; ++i) {
+		int interactions = 17;
+		if ( MODE == 0 ) {
+			interactions = 100_000;
+		}
+		for (int i = 0; i < interactions; ++i) {
 			final int iInner = i;
 			pool.submit(() -> {
 				try {
-					KeySpec spec = new PBEKeySpec(password.toCharArray(), pin.getBytes(), iInner, 256);
-					SecretKey key = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+					
+					byte[] bytes;
+					if ( MODE == 0 ) {
+						KeySpec spec = new PBEKeySpec(password.toCharArray(), pin.getBytes(), iInner, 256);
+						bytes = factory.generateSecret(spec).getEncoded();
+					} else if ( MODE == 1 ) {
+						bytes = BCrypt.generate(
+								password.getBytes(), // byte array of user supplied, low entropy passw 
+								Arrays.copyOf(pin.getBytes(), 16), // 128 bit(= 16 bytes), CSPRNG generated salt
+					            iInner // cost factor, performs 2^14 iterations.
+					            );
+					}  else {
+						bytes = SCrypt.generate(
+								password.getBytes(), // user supplied password, converted into byte array
+								Arrays.copyOf(pin.getBytes(), 16), // salt of size 32 bytes
+					            (int)Math.pow(2, iInner), // CPU/Memory cost
+					            16, // block size
+					            1, // Parallelization parameter:
+					            32 // (256 bits) Length of output key size
+					            );
+					}
+					
+					SecretKey key = new SecretKeySpec(bytes, "AES");
 
 					Cipher cipher;
 					cipher = Cipher.getInstance(AESUtil.CIPHER_INSTANCE);
@@ -169,8 +216,22 @@ public class TesteAleatorio implements CommandLineRunner {
 					boolean twice = Boolean.FALSE, base58 = Boolean.FALSE, base64 = Boolean.FALSE;
 
 					try {
-						cipher.doFinal(decryptedBytes);
+						@SuppressWarnings("unused")
+						byte[] innerBytes = cipher.doFinal(Arrays.copyOfRange(blockAes, blockAes.length - 224, blockAes.length));
 						twice = Boolean.TRUE;
+						if ( MODE != 0 ) {
+							// trying to decrypt inner block
+//							System.out.println(String.format("Inner bytes (length: %d): \"%s\"", innerBytes.length, new String(innerBytes, guessEncoding(innerBytes))));
+							
+//							try {
+//								System.out.println("Base58: " + Base58.decode(new String(decryptedBytes)));
+//							} catch (Exception ex) {
+//							}
+//							try {
+//								System.out.println("Base64: " + Base64.getDecoder().decode(new String(decryptedBytes)));
+//							} catch (Exception ex) {
+//							}
+						}
 					} catch (Exception ex) {
 					}
 					try {
@@ -184,23 +245,27 @@ public class TesteAleatorio implements CommandLineRunner {
 					} catch (Exception ex) {
 					}
 
-					// System.out.println(
-					// String.format("Pass = %s, Pin = %s, I = %d, Reversed? %s,
+					// System.out.println(String.format("Pass = %s, Pin = %s, I = %d, Reversed? %s,
 					// Twice? %s\nDecrypted: %s",
 					// password, pin, iInner, reversed, twice, decrypted));
 
 					try {
-					System.out.println(String.format("%s;%s;%d;%s;%s;%s;%s;%d", password, pin,
+						System.out.println(String.format("%s;%s;%d;%s;%s;%s;%s;%d", password, pin,
 							iInner, reversed, twice, base58, base64, decryptedBytes.length));
+//						if ( MODE != 0 ) {
+//							System.out.println(String.format("Decrypted (length: %d): \"%s\"", decryptedBytes.length,
+//									new String(decryptedBytes, guessEncoding(decryptedBytes))));
+//						}
 					} catch (Exception ex) {
 						ex.printStackTrace();
 					}
 
 				} catch (Exception ex) {
+					//ex.printStackTrace();
 				}
 			});
 
-			while (queue.size() > 100_000) {
+			while (queue.size() > interactions) {
 				TimeUnit.MILLISECONDS.sleep(100);
 			}
 		}
